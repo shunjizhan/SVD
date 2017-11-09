6a572936113bec129a6dcb3e6ea40f7fc7887835
Shunji Zhan <shunhahaha@live.com>
2017-11-08 17:18:20 -0800

    works

--- svd.py
+++ svd.py
@@ -1,12 +1,13 @@
 from scipy import misc
 from PIL import Image
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def split(x, w, h):
-    R = numpy.zeros(shape=(w, h))
-    G = numpy.zeros(shape=(w, h))
-    B = numpy.zeros(shape=(w, h))
+    R = np.zeros(shape=(w, h))
+    G = np.zeros(shape=(w, h))
+    B = np.zeros(shape=(w, h))
     for i in range(0, w):
         for j in range(0, h):
             R[i][j] = x[i][j][0]
@@ -16,10 +17,37 @@ def split(x, w, h):
     return R, G, B
 
 
-if __name__ == "__main__":
-    im = Image.open('pic.png')
-    width, height = im.size
+def combine(R, G, B, w, h):
+    result = np.zeros(shape=(w, h, 3))
+    for i in range(0, w):
+        for j in range(0, h):
+            result[i][j][0] = R[i][j]
+            result[i][j][1] = G[i][j]
+            result[i][j][2] = B[i][j]
 
-    arr = misc.imread('pic.png')  # 640x480x3 array
-    R, G, B = split(arr, width, height)
-    
+    return result
+
+
+def best_k(A, k):
+    U, s, V = np.linalg.svd(A, full_matrices=True)
+    U_new = U[:, :k]
+    S_new = np.diag(s[:k])
+    V_new = V[:k, :]
+
+    return (U_new.dot(S_new)).dot(V_new)
+
+
+if __name__ == "__main__":
+    pic = 'pic2.png'
+    im = Image.open(pic)
+    h, w = im.size
+
+    arr = misc.imread(pic)  # 640x480x3 array
+    R, G, B = split(arr, w, h)
+    k = 10
+    R_best, G_best, B_best = best_k(R, k), best_k(G, k), best_k(B, k)
+
+    rbg_new = combine(R_best, G_best, B_best, w, h)
+    # print rbg_new.shape
+    plt.imshow(np.uint8(rbg_new))
+    plt.show()
